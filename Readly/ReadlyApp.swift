8d1271849f768811197a623d995dddd55b99009e
Bryan <71095749+bryanless@users.noreply.github.com>
2023-04-04 10:19:58 +0700

    Initial Commit

---
+++ Readly/ReadlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadlyApp.swift
+//  Readly
+//
+//  Created by Bryan on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ReadlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
